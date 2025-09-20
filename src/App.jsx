5bf8324ac8634496b65d6e2618e203337c5c57de
Supun Fernando <supuncharindu18@gmail.com>
2025-09-20 09:55:24 +0200

    Make new function for roll button roll-dice function it will chage state of dice when click it basically change setDice arra to random num array

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from "react";
 import Die from "./Die";
 
 export default function App() {
+  // State: array of dice values
+  const [dice, setDice] = useState(GenerateAllNewDice);
+
   // Generates an array of 10 random numbers between 1 and 6
   function GenerateAllNewDice() {
     return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
   }
 
-  // State: array of dice values
-  const [dice, setDice] = useState(GenerateAllNewDice);
+  //roll dice button function
+  function rollDice(){
+    setDice(GenerateAllNewDice())
+  }
+
 
   // Map each die value to a Die component
   const diceElement = dice.map((num) => <Die value={num} />);
@@ -16,6 +22,7 @@ export default function App() {
   return (
     <main>
       <div className="die-container">{diceElement}</div>
+      <button className="roll-dice" onClick={rollDice}>Roll</button>
     </main>
   );
 }
