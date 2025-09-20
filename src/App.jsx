5e4f92ecb31aae256d10e488430d44c1800ad355
Supun Fernando <supuncharindu18@gmail.com>
2025-09-20 09:25:55 +0200

    Make function to chose random number between 1-6 for 10 dice as an array

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import Die from "./Die";
 
+function GenerateAllNewDice() {
+  return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+}
+
+console.log(GenerateAllNewDice());
+
 export default function App() {
   return (
     <main>
